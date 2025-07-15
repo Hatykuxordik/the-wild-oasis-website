5be388687cd6ae24db6e79e5069a4fbd30002a89
Hatykuxordik <hatykuxordik@gmail.com>
2025-07-15 16:10:43 +0100

    Fetch and display CabinCard

---
+++ src/app/cabins/loading.tsx
@@ -0,0 +1,10 @@
+import Spinner from "@/app/_components/Spinner";
+
+export default function Loader(): JSX.Element {
+  return (
+    <div className="grid items-center justify-items-center">
+      <Spinner />
+      <p className="text-xl text-primary-200">Loading cabin data...</p>
+    </div>
+  );
+}
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,5 @@
+import Spinner from "@/app/_components/Spinner";
+
 export default function Loader(): JSX.Element {
-  return <p>LOADING DATA...</p>;
+  return <Spinner />;
 }
