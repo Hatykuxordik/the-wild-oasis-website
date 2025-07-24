129d99832f88916afe70440ab220df5c971647c9
Hatykuxordik <hatykuxordik@gmail.com>
2025-07-24 19:53:22 +0100

    Add Static Site Generator

--- src/app/cabins/[cabinid]/page.tsx
+++ src/app/cabins/[cabinid]/page.tsx
@@ -62,6 +62,10 @@ export default async function Page({ params }: Props) {
         </div>
 
         <div>
+          <h3 className="text-accent-100 font-black text-7xl mb-5 translate-x-[-254px] bg-primary-950 p-6 pb-1 w-[150%]">
+            Cabin {name}
+          </h3>
+
           <p className="text-lg text-primary-300 mb-10">{description}</p>
 
           <ul className="flex flex-col gap-4 mb-7">
