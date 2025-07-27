b9a484789d37b8a8ccb5f29841908bb5da7b56b9
Hatykuxordik <hatykuxordik@gmail.com>
2025-07-27 20:40:19 +0100

    Add mobile version to [cabinId] page

--- src/app/cabins/[cabinId]/page.tsx
+++ src/app/cabins/[cabinId]/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import Image from "next/image";
 import NotFound from "./not-found";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
+import Link from "next/link";
 
 type Props = {
   params: Promise<{
@@ -60,9 +61,33 @@ export default async function Page({ params }: Props) {
   const { name, maxCapacity, image, description } = cabin;
 
   return (
-    <div className="max-w-6xl mx-auto mt-8">
-      <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
-        <div className="relative scale-[1.15] -translate-x-3 h-[400px]">
+    <div className="w-full md:max-w-6xl mx-2 md:mx-auto mt-8">
+      <Link
+        href="/cabins"
+        className="md:hidden absolute top-30 left-2 z-20 text-primary-100 hover:text-accent-400 transition-colors"
+        aria-label="Back to cabins"
+      >
+        <svg
+          className="w-6 h-6 text-primary-100"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="3"
+            d="M5 12h14M5 12l4-4m-4 4 4 4"
+          />
+        </svg>
+      </Link>
+
+      <div className=" flex flex-col md:grid md:grid-cols-[3fr_4fr] gap-10 md:gap-20 border border-primary-800 py-3 px-4 md:px-10 mb-12 md:mb-24">
+        <div className="relative md:scale-[1.15] md:translate-x-3 h-[400px]">
           <Image
             src={image}
             fill
@@ -72,7 +97,7 @@ export default async function Page({ params }: Props) {
         </div>
 
         <div>
-          <h3 className="text-accent-100 font-black text-7xl mb-5 translate-x-[-254px] bg-primary-950 p-6 pb-1 w-[150%]">
+          <h3 className="text-accent-100 font-black text-4xl md:text-7xl mb-3 md:mb-5 translate-x-[-65px] md:translate-x-[-254px] bg-primary-950 p-4 md:p-6 pb-1 md:w-[150%]">
             Cabin {name}
           </h3>
 
@@ -104,8 +129,9 @@ export default async function Page({ params }: Props) {
       </div>
 
       <div>
-        <h2 className="text-5xl font-semibold text-center">
-          Reserve today. Pay on arrival.
+        <h2 className="text-3xl md:text-5xl font-semibold text-center">
+          Reserve today.{" "}
+          <span className="block md:inline">Pay on arrival.</span>
         </h2>
       </div>
     </div>
