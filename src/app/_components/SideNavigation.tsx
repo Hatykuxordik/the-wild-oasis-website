dcb0f3efd09fd8282fdeba51af67ed5a89a09f22
Hatykuxordik <hatykuxordik@gmail.com>
2025-07-28 21:31:48 +0100

    Add usePathname, NEXT.js custom hook to add active to a  link

--- src/app/_components/SideNavigation.tsx
+++ src/app/_components/SideNavigation.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   CalendarDaysIcon,
   HomeIcon,
@@ -6,6 +8,7 @@ import {
 import SignOutButton from "./SignOutButton";
 import { JSX } from "react"; // Needed for proper typing of React nodes
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type NavLink = {
   name: string;
@@ -32,13 +35,16 @@ const navLinks: NavLink[] = [
 ];
 
 function SideNavigation() {
+  const pathname = usePathname();
   return (
     <nav className="border-r border-primary-900">
       <ul className="flex flex-col gap-2 h-full text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
-              className="py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200"
+              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${
+                pathname === link.href ? "bg-primary-900" : ""
+              }`}
               href={link.href}
             >
               {link.icon}
