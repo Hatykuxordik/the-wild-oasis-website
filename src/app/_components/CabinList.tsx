d3a3ac538188ce9209df98c1115fc64228360145
Hatykuxordik <hatykuxordik@gmail.com>
2025-07-29 06:07:32 +0100

    Add searchParams for sending from client to server

--- src/app/_components/CabinList.tsx
+++ src/app/_components/CabinList.tsx
@@ -15,15 +15,29 @@ type Cabin = {
   discount: number;
 };
 
-async function CabinList() {
+async function CabinList({ filter }) {
   //noStore();  Ensure this data is not cached
   // 2️⃣ Type the array correctly
   const cabins: Cabin[] = await getCabins();
 
   if (!cabins.length) return null;
+
+  let displayedCabins;
+  if (filter === "all") displayedCabins = cabins;
+  if (filter === "small")
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  if (filter === "medium")
+    displayedCabins = cabins.filter(
+      (cabin) => cabin.maxCapacity <= 4 && cabin.maxCapacity <= 7
+    );
+  if (filter === "large")
+    displayedCabins = cabins.filter(
+      (cabin) => cabin.maxCapacity <= 8 && cabin.maxCapacity >= 5
+    );
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
--- src/app/cabins/page.tsx
+++ src/app/cabins/page.tsx
@@ -7,7 +7,15 @@ export const metadata: Metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+interface SearchParamsProps {
+  searchParams: {
+    [key: string]: string | undefined;
+  };
+}
+
+export default function Page({ searchParams }: SearchParamsProps) {
+  const filter = searchParams?.capacity || "all";
+
   return (
     <div>
       <h1 className="text-3xl md:text-4xl mb-2 md:mb-5 text-accent-400 font-medium">
@@ -24,7 +32,7 @@ export default function Page() {
 
       <Suspense fallback={<Spinner />}>
         {/* Suspense is used to handle the loading state of the CabinList component */}
-        <CabinList />
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
